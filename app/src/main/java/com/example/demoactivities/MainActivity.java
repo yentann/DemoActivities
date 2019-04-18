4069ae0014cb9c3d2fe4d5aecb2d1adb61cb91e8
TAN QIAO YUE <17045679@rp.edu.sg>
2019-04-18 20:26:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoactivities/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.demoactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Name;
+    EditText Age;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = findViewById(R.id.editTextName);
+        Age = findViewById(R.id.editTextAge);
+
+
+    }
+}
